8f81826dcfbc19eec5858b6153be8f7a3f046889
minsoku <minsoku03@gmail.com>
2023-12-17 13:37:54 +0900

    Title: a 부터 b 까지, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231217/a 부터 b 까지/a-to-b.py
+++ 231217/a 부터 b 까지/a-to-b.py
@@ -2,11 +2,13 @@ a, b = map(int, input().split())
 arr = [str(a)]
 i = a
 while i < b:
-    if i < b and i % 2 == 1:
+    if i % 2 == 1:
         i *= 2
-        arr.append(str(i))
-    if i < b and i % 2 == 0:
+        if i < b:
+            arr.append(str(i))
+    if  i % 2 == 0:
         i += 3
-        arr.append(str(i))
+        if i < b:
+            arr.append(str(i))
 
 print(' '.join(arr))
